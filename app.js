8c97993d66e13959ce47dc2227b3e98e5c06f0f5
lytruongsinh123 <diepduong607@gmail.com>
2025-08-12 22:56:45 +0700

    Change

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 const { MONGOURI } = require("./config/keys");
+const cors = require("cors");
+
 mongoose.set("strictQuery", true);
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
@@ -20,6 +22,12 @@ require("./models/user");
 require("./models/post");
 
 app.use(express.json());
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL, // thay bằng domain thật của bạn trên Vercel
+        credentials: true,
+    })
+);
 app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));
