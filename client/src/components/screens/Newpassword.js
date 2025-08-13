7613b0755a33ec6cc0eeb13cb2901e9412e91a80
lytruongsinh123 <diepduong607@gmail.com>
2025-08-13 16:07:56 +0700

    change

--- client/src/components/screens/Newpassword.js
+++ client/src/components/screens/Newpassword.js
@@ -1,57 +1,65 @@
-import React,{useState,useContext,} from 'react'
-import {Link,useHistory,useParams} from 'react-router-dom'
-import M from 'materialize-css'
-const SignIn  = ()=>{
-    const history = useHistory()
-    const [password,setPasword] = useState("")
-    const {token} = useParams()
-    console.log(token)
-    const PostData = ()=>{
-        fetch("https://hung-instagram.onrender.com/new-password",{
-            method:"post",
-            headers:{
-                "Content-Type":"application/json"
+import React, { useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import M from "materialize-css";
+import "../../styles/newpassword.css";
+const NewPassword = () => {
+    const history = useHistory();
+    const [password, setPasword] = useState("");
+    const { token } = useParams();
+    console.log(token);
+    const PostData = () => {
+        fetch("https://hung-instagram.onrender.com/new-password", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
             },
-            body:JSON.stringify({
+            body: JSON.stringify({
                 password,
-                token
-            })
-        }).then(res=>res.json())
-        .then(data=>{
-            console.log(data)
-           if(data.error){
-              M.toast({html: data.error,classes:"#c62828 red darken-3"})
-           }
-           else{
-
-               M.toast({html:data.message,classes:"#43a047 green darken-1"})
-               history.push('/signin')
-           }
-        }).catch(err=>{
-            console.log(err)
+                token,
+            }),
         })
-    }
-   return (
-      <div className="mycard">
-          <div className="card auth-card input-field">
-            <h2>Instagram</h2>
-        
-            <input
-            type="password"
-            placeholder="enter a new password"
-            value={password}
-            onChange={(e)=>setPasword(e.target.value)}
-            />
-            <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
-            onClick={()=>PostData()}
-            >
-               Update password
-            </button>
-    
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                if (data.error) {
+                    M.toast({
+                        html: data.error,
+                        classes: "#c62828 red darken-3",
+                    });
+                } else {
+                    M.toast({
+                        html: data.message,
+                        classes: "#43a047 green darken-1",
+                    });
+                    history.push("/signin");
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    return (
+        <div className="newpassword-container">
+            <div className="newpassword-title">Instagram</div>
+            <form
+                className="newpassword-form"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    PostData();
+                }}>
+                <input
+                    className="newpassword-input"
+                    type="password"
+                    placeholder="Enter a new password"
+                    value={password}
+                    onChange={(e) => setPasword(e.target.value)}
+                />
+                <button type="submit" className="newpassword-btn">
+                    Update password
+                </button>
+            </form>
         </div>
-      </div>
-   )
-}
+    );
+};
 
-
-export default SignIn
+export default NewPassword;
